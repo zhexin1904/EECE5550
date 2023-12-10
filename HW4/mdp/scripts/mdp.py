a696da4b2c30c8ae633bf9bfcc84354e03292dbf
zhexinXU <72389598+zhexin1904@users.noreply.github.com>
2023-12-11 03:30:48 +0800

    Update readme

--- HW4/mdp/scripts/mdp.py
+++ HW4/mdp/scripts/mdp.py
@@ -4,13 +4,14 @@ import pdb
 
 
 
-class MDPValueIteration:
+class MDP:
     def __init__(self, map, test, factor, actionType, plans):
         self.map = map
-        self.test = test
-        self.factor = factor
         self.actionType = actionType
         self.plans = plans
+        self.test = test
+        self.factor = factor
+
 
     def initialize_value_function(self):
         return np.zeros_like(self.map)
@@ -25,7 +26,6 @@ class MDPValueIteration:
         return {'test_1': 0.1, 'test_2': 0.4, 'factor': 0.9, 'actionType': ['up', 'down', 'right', 'left'],
                 'plans': {'up': (-1, 0), 'down': (1, 0), 'right': (0, 1), 'left': (0, -1)}}
 
-
     def update(self):
         value = self.initialize_value_function()
         delta_threshold = self.test * (1 - self.factor) / self.factor
@@ -33,7 +33,6 @@ class MDPValueIteration:
         while True:
             delta = 0
             updated_value = np.copy(value)
-
             for i in range(self.map.shape[0]):
                 for j in range(self.map.shape[1]):
                     if self.map[i, j] != -0.01:
@@ -41,40 +40,34 @@ class MDPValueIteration:
                     # pdb.set_trace()
                     updated_value[i, j] = max(self.q_value((i, j), a, value) for a in self.actionType)
                     delta = max(delta, abs(value[i, j] - updated_value[i, j]))
-
             value = np.copy(updated_value)
-
             if delta < delta_threshold:
                 break
 
         return value
 
-
-
-    def q_value(self, state, action, V):
-        next_states = self.get_next_states(state, action)
-        q = sum(self.decision(action, s_prime, state) * V[s_prime[0], s_prime[1]] for s_prime in next_states)
-        return q
-
-    def get_next_states(self, state, action):
+    def futurestates(self, state, action):
         i, j = state
         if action in self.plans:
-            i_prime, j_prime = i + self.plans[action][0], j + self.plans[action][1]
-            return [(i_prime, j_prime)] if 0 <= i_prime < self.map.shape[0] and 0 <= j_prime < self.map.shape[1] else [(i, j)]
+            i_spot, j_spot = i + self.plans[action][0], j + self.plans[action][1]
+            return [(i_spot, j_spot)] if 0 <= i_spot < self.map.shape[0] and 0 <= j_spot < self.map.shape[1] else [(i, j)]
         else:
             return [(i, j)]
 
-
+    def value_(self, state, action, V):
+        future_states = self.futurestates(state, action)
+        value = sum(self.decision(action, spot, state) * V[spot[0], spot[1]] for spot in future_states)
+        return value
 
     def decision(self, action, state_prime, state):
         i, j = state
-        i_prime, j_prime = state_prime
+        i_spot, j_spot = state_spot
         if action in ['up', 'down', 'right', 'left']:
-            return 1 - self.test if (i_prime - i, j_prime - j) == self.plans[action] else self.test / 2
+            return 1 - self.test if (i_spot - i, j_spot - j) == self.plans[action] else self.test / 2
         else:
             return 0 
         
-    def optimal_policy(self, V):
+    def best_decision(self, V):
         policy = np.empty_like(self.map, dtype='<U5')
         for i in range(self.map.shape[0]):
             for j in range(self.map.shape[1]):
@@ -88,12 +81,12 @@ class MDPValueIteration:
         plt.imshow(self.map, cmap='viridis', origin='upper', extent=[0, self.map.shape[1], 0, self.map.shape[0]])
         for i in range(self.map.shape[0]):
             for j in range(self.map.shape[1]):
-                self.plot_arrow(policy[i, j], j + 0.5, self.map.shape[0] - (i + 0.5))
+                self.visualize_direction(policy[i, j], j + 0.5, self.map.shape[0] - (i + 0.5))
         plt.title(title)
         plt.show()
         plt.savefig('1.png', dpi=300)
 
-    def plot_arrow(self, action, x, y):
+    def visualize_direction(self, action, x, y):
         if action == 'up':
             plt.arrow(x, y, 0, 0.3, head_width=0.1, head_length=0.1, fc='red', ec='red')
         elif action == 'down':
@@ -104,16 +97,16 @@ class MDPValueIteration:
             plt.arrow(x, y, -0.3, 0, head_width=0.1, head_length=0.1, fc='red', ec='red')
 
 def main():
-    mdp_vi = MDPValueIteration(None, None, None, None, None)
+    mdp_vi = MDP(None, None, None, None, None)
     map = mdp_vi.initialize_map()
     parameters = mdp_vi.initialize_parameters()
 
-    mdp_vi = MDPValueIteration(map, parameters['test_2'], parameters['factor'], parameters['actionType'], parameters['plans'])
+    mdp_vi = MDP(map, parameters['test_2'], parameters['factor'], parameters['actionType'], parameters['plans'])
     value = mdp_vi.update()
-    test_1 = mdp_vi.optimal_policy(value)
+    test_1 = mdp_vi.best_decision(value)
 
     print("Final value:\n", value)
-    print("Optimal Policy :\n", test_1)
+    print("Best :\n", test_1)
 
     mdp_vi.visualize(test_1, 'ϵ = 0.4')
 

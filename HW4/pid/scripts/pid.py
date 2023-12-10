1296c7234ce032f96a54b2cb4504b4b4718a1f61
zhexinXU <72389598+zhexin1904@users.noreply.github.com>
2023-12-11 03:14:26 +0800

    update readme

--- HW4/pid/scripts/pid.py
+++ HW4/pid/scripts/pid.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 class PIDController:
     def __init__(self, Kp, Ki, Kd, discont):
         self.m = 0.065  
-        self.kT = 5.276e-4  
-        self.g = 9.81  
         self.r = 1  
         self.h = 0  
         self.velo = 0  
+        self.kT = 5.276e-4  
+        self.g = 9.81  
         self.dt = 0.05  
         self.time = np.arange(0, 150, self.dt)
         self.Kp = Kp
@@ -23,13 +23,11 @@ class PIDController:
         for _ in self.time:
             error = self.r - self.h
             self.integral += error * self.dt
-            u = self.Kp * error + self.Ki * self.integral + self.Kd * (-self.velo) + (self.m * self.g) / (4 * self.kT)
-            u_actual = self.discont * u  
-            # System dynamics
-            acc = (4 * self.kT * u_actual - self.m * self.g) / self.m
+            output = self.Kp * error + self.Ki * self.integral + self.Kd * (-self.velo) + (self.m * self.g) / (4 * self.kT)
+            real_output = self.discont * output  
+            acc = (4 * self.kT * real_output - self.m * self.g) / self.m
             self.velo += acc * self.dt
             self.h += self.velo * self.dt
-            # Store results
             self.altitude_pid.append(self.h)
             self.velocity_pid.append(self.velo)
 
@@ -46,13 +44,8 @@ class PIDController:
         plt.show()
 
 def main():
-    # Create an instance of the PIDControlSystem class
     system = PIDController(10, 1.8, 14, 0.95)
-
-    # Run the simulation
     system.intergartion()
-
-    # Plot results
     system.plot_results()
 
 if __name__ == "__main__":

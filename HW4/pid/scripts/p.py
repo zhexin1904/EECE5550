9813def5857f690c228ac220bf818451ce1e4da7
zhexinXU <72389598+zhexin1904@users.noreply.github.com>
2023-12-11 03:19:00 +0800

    Update readme

--- HW4/pid/scripts/p.py
+++ HW4/pid/scripts/p.py
@@ -6,27 +6,27 @@ class Pcontroller:
         self.m = 0.065  
         self.g = 9.81  
         self.kT = 5.276e-4
-        self.r = 1  
+        self.ref = 1  
         self.Kp = Kp
         self.dt = 0.05
-        self.time = np.arange(0, 100, self.dt)
-        self.h = 0
+        self.altitude = 0
         self.velo = 0
-        self.h_list = []
+        self.time = np.arange(0, 100, self.dt)
+        self.altitude_list = []
         self.velo_list = []
 
     def intergartion(self):
         for _ in self.time:
-            u = self.Kp * (self.r - self.h) + (self.m * self.g) / (4 * self.kT)
-            velo_dot = (4 * self.kT * u - self.m * self.g) / self.m
+            output = self.Kp * (self.r - self.altitude) + (self.m * self.g) / (4 * self.kT)
+            velo_dot = (4 * self.kT * output - self.m * self.g) / self.m
             self.velo += velo_dot * self.dt
-            self.h += self.velo * self.dt
-            self.h_list.append(self.h)
+            self.altitude += self.velo * self.dt
+            self.altitude_list.append(self.altitude)
             self.velo_list.append(self.velo)
 
     def visualize(self):
         plt.figure()
-        # plt.plot(self.time, self.h_list, linewidth=2, color='r')
+        # plt.plot(self.time, self.altitude_list, linewidth=2, color='r')
 
         # plt.xlabel('Timestamp')
         # plt.ylabel('Hieght/m')
@@ -47,7 +47,6 @@ def main():
     # system2.intergartion()
     system3.intergartion()
 
-    # Plot 
     # system1.visualize()
     # system2.visualize()
     system3.visualize()

6bb7b9aabe1514f40ba768de2e874a0ffe3101b3
zhexinXU <72389598+zhexin1904@users.noreply.github.com>
2023-12-11 03:22:12 +0800

    Update readme

--- HW4/pid/scripts/pid.py
+++ HW4/pid/scripts/pid.py
@@ -4,36 +4,36 @@ import matplotlib.pyplot as plt
 class PIDController:
     def __init__(self, Kp, Ki, Kd, discont):
         self.m = 0.065  
-        self.r = 1  
-        self.h = 0  
+        self.ref = 1  
+        self.altitude = 0  
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
         self.velo = 0  
         self.kT = 5.276e-4  
         self.g = 9.81  
         self.dt = 0.05  
         self.time = np.arange(0, 150, self.dt)
-        self.Kp = Kp
-        self.Ki = Ki
-        self.Kd = Kd
         self.discont = discont
         self.altitude_pid = []
         self.velocity_pid = []
-        self.integral = 0
+        self.integration = 0
 
     def intergartion(self):
         for _ in self.time:
-            error = self.r - self.h
-            self.integral += error * self.dt
-            output = self.Kp * error + self.Ki * self.integral + self.Kd * (-self.velo) + (self.m * self.g) / (4 * self.kT)
+            error = self.r - self.altitude
+            self.integration += error * self.dt
+            output = self.Kp * error + self.Ki * self.integration + self.Kd * (-self.velo) + (self.m * self.g) / (4 * self.kT)
             real_output = self.discont * output  
             acc = (4 * self.kT * real_output - self.m * self.g) / self.m
             self.velo += acc * self.dt
-            self.h += self.velo * self.dt
-            self.altitude_pid.append(self.h)
+            self.altitude += self.velo * self.dt
+            self.altitude_pid.append(self.altitude)
             self.velocity_pid.append(self.velo)
 
-    def plot_results(self):
+    def visualize(self):
         # plt.plot(self.time, self.altitude_pid, label='Height / m', linewidth=2, color='r')
-        # plt.axhline(y=self.r, color='b', linestyle='--', label='ref')
+        # plt.axhline(y=self.ref, color='b', linestyle='--', label='ref')
         # plt.legend()
         # plt.xlabel('Timestamp')
         # plt.title('PID')
@@ -46,7 +46,7 @@ class PIDController:
 def main():
     system = PIDController(10, 1.8, 14, 0.95)
     system.intergartion()
-    system.plot_results()
+    system.visualize()
 
 if __name__ == "__main__":
     main()
